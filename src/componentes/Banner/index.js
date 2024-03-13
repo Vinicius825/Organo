89420b38a4f1618938d0d5463d96c24c7a817182
Vinicius Santo <vinicius.santo@amchambrasil.com.br>
2024-03-13 17:08:49 -0300

    Novo Projeto React

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+const Banner = () =>{
+    return (
+        <header className="banner">
+        <img src={require('../../assets/imagens/banner.png')} alt="Banner principal da pagina do Organo"/>
+        </header>
+    )
+}
+export default Banner
